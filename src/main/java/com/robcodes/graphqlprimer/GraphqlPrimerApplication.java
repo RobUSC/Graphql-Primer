64c718665be308d0bded2dceaee541bd0370197b
Rob <Robertcarterusc@gmail.com>
2021-02-21 08:45:10 -0500

    init
    init spring

---
+++ src/main/java/com/robcodes/graphqlprimer/GraphqlPrimerApplication.java
@@ -0,0 +1,13 @@
+package com.robcodes.graphqlprimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPrimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlPrimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robcodes/graphqlprimer/GraphqlPrimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robcodes.graphqlprimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPrimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
